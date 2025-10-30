7969a224929bba031bb89294ca7e4e0ff870719a
Avinashreddy721 <165068217+Avinashreddy721@users.noreply.github.com>
2025-10-30 17:05:12 +0530

    updated : status in home page

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -12,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target:'http://127.0.0.1:8000',
+        target:'http://127.0.1.1:8000',
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, '')
       }
